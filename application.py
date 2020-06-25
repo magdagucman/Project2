c30ccf7ce54336d4afab4708ebdf2977cac54e1e
Magda <58512899+magdagucman@users.noreply.github.com>
2020-06-25 14:19:41 +0100

    Update application.py

--- application.py
+++ application.py
@@ -23,43 +23,96 @@ def after_request(response):
     return response
 
 
-# Store users, channels and posts as global variables
+# Store users and channels as global variables
 users = []
-channels = ["Dogs", "Cats"]
-posts = []
+channels = []
+
+# Append first two channels
+channels.append(Channel(name="Dogs"))
+channels.append(Channel(name="Cats"))
 
 @app.route("/")
 def index():
-    return render_template("index.html", channels=channels, posts=posts)
+    return render_template("index.html", channels=channels)
 
-@app.route("/<string:channel>")
-def channel(channel):
-    return channel
+@app.route("/delete", methods=['POST'])
+def delete():
+    delete_channel = request.form.get("delete_channel")
+    delete_username = request.form.get("delete_username")
+    delete_post = request.form.get("delete_post")
+
+    # Iterate through channels
+    for channel in channels:
+
+        # When right channel is found, iterate through its posts
+        if channel.name == delete_channel:
+            for post in channel.posts:
+
+                # If username and message match the deleted by user, remove the whole post
+                if post.message == delete_post and post.username == delete_username:
+                    channel.posts.remove(post)
+                    return jsonify({"success": True})
+    return jsonify({"success": False})
+
+@app.route("/<string:name>")
+def channel(name):
+    posts_list = []
+    # Iterate through channels
+    for channel in channels:
+        if channel.name == name:
+
+            # When right channel is found, iterate through posts
+            for post in channel.posts:
+
+                # Append each post to post list
+                posts_list.append([post.username, post.message, post.time])
+    return jsonify({"channel": name, "posts": posts_list})
 
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
     username = request.form.get("username")
+
+    # If username is taken, return False
     if username in users:
         return jsonify({"success": False})
+
+    # If username is free to take, append it to users list and return True
     users.append(username)
     return jsonify({"success": True, "username": username})
 
 
 @app.route("/get_channel", methods=["GET"])
 def get_channel():
-    return jsonify({"success": True, "channels": channels})
+    channel_list = []
 
+    # Iterate through channels and append each one to channel list
+    for channel in channels:
+        channel_list.append(channel.name)
+    return jsonify({"success": True, "channels": channel_list})
 
+# Add newly created channels in real time
 @socketio.on("add channel")
 def add_channel(data):
     selection = data["selection"].capitalize()
-    channels.append(selection)
+    channel = Channel(name=selection)
+    channels.append(channel)
     emit("display channel", {"selection": selection}, broadcast=True)
 
+# Add newly created posts in real time
 @socketio.on("add post")
 def add_post(data):
-    post = Post(channel=data["selection"][0], username=data["selection"][1], message=data["selection"][2], time=datetime.datetime.now())
-    posts.append(post)
-    selection = data["selection"]
+    post = Post(channel=data["selection"][0], username=data["selection"][1], message=data["selection"][2], time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    for channel in channels:
+
+        # If there are less than 100 posts per channel, append post
+        if channel.name == data["selection"][0] and len(channel.posts) < 100:
+            channel.posts.append(post)
+
+        # If there are more than 100 posts per channel, first remove the oldest post, and then append the newest
+        elif channel.name == data["selection"][0] and len(channel.posts) >= 100:
+            channel.posts.pop(0)
+            channel.posts.append(post)
+
+    selection = [post.username, post.message, post.time, post.channel]
     emit("display post", {"selection": selection}, broadcast=True)
